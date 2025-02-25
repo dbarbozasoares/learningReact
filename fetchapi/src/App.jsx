a0ec76a07a3dc29d05982bfc0881f6befafbec53
dbarbozasoares <dbarboza.soares@gmail.com>
2025-02-25 11:27:12 -0500

    FETCH API AND REACT

---
+++ fetchapi/src/App.jsx
@@ -0,0 +1,33 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+function App() {
+  const [products, setProducts] = useState([]);
+
+  const url = "http://localhost:3000/products";
+  // get data
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url);
+      const data = await res.json();
+      setProducts(data);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <div className="App">
+        <h1>Lista de Produtos</h1>
+        <ul>
+          {products.map((product) => (
+            <li key={product.id}>
+              {product.name} - R${product.price}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default App;
